fc263e595029947b70224b47460f68954626c6c9
cesar438 <148093778+cesar438@users.noreply.github.com>
2024-05-16 19:10:43 -0400

    autenticacion

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //importacion de libs
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const authRutas = require('./rutas/authRutas');
+const Usuario = require('./models/Usuario');
 require('dotenv').config();
 const app = express();
 // llamar rutas
@@ -19,6 +22,25 @@ mongoose.connect(MONGO_URI).then(
         app.listen(PORT, () => {console.log("Servidor express corriendo en el puerto: "+PORT)})
     }
 ).catch( error => console.log('error de conexion', error));
+//
+const autenticar = async (req, res, next)=>{
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token)
+            res.status(401).json({mensaje: 'No existe el token de autenticacion'});
+        const decodificar = jwt.verify(token, 'clave_secreta');
+        req.usuario = await  Usuario.findById(decodificar.usuarioId);
+        next();
+    }
+    catch(error){
+        res.status(400).json({ error: 'Token invalido!!'});
+    }
+};
+
+app.use('/auth', authRutas);
+app.use('/recetas', autenticar, recetaRutas);
 
 //utilizar las rutas de recetas
-app.use('/recetas', recetaRutas);
+//app.use('/recetas', recetaRutas);
+
+
